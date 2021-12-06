3e44cd6650baeaf6b5c69574452471e7f9a95a82
+ <+>
2021-12-06 14:48:44 -0500

    week 1 day 1 rb

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,5 +1,9 @@
 def reverse_string(str)
-  # type your code in here
+  backwards_string = ""
+  str.chars.each do |letter|
+    backwards_string = letter + backwards_string
+  end
+  backwards_string
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -12,7 +16,17 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", reverse_string('catbaby')
 
   # Don't forget to add your own!
+  puts
+
+  puts "Expecting: 'tacocat ekil"
+  puts "=>", reverse_string('like tacocat')
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# Pseudocode:
+# iterate over each letter, add to a string, then add up
+
+# Explanation: 
+# Take each letter, store it, then as go through string will be stored in reverse
