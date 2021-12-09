4937c8bbdcaae47327a05093f2b752858c9c0dd6
+ <+>
2021-12-08 22:48:47 -0500

    week 1 day 2 rb

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,5 +1,12 @@
+require 'set'
 def find_first_duplicate(arr)
-  # type your code in here
+  noDuplicates = Set.new 
+
+  arr.each do |val|
+    return val if noDuplicates.include?(val)
+    noDuplicates.add(val)
+  end
+  -1
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,6 +18,15 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
 
+  puts
+
+  puts "Expecting: -1"
+  puts "=>", find_first_duplicate([8, 7, 7, 9]);
+
+  puts
+
+  puts "Expecting: -1"
+  puts "=>", find_first_duplicate([4, 3, 2, 1]);
   # Don't forget to add your own!
 end
 
