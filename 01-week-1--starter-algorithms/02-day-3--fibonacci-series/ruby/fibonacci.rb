b949fdd6a7a543caee806dca343dcda49a22baf2
+ <+>
2021-12-09 20:53:27 -0500

    week 1 day 3 rb

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,11 @@
 def fibonacci(num)
-  # type your code in here
+  two_num = [0, 1]
+  return two_num[num] if num < 2
+  (num - 1).times do
+    sum = two_num[0] + two_num[1]
+    two_num = [two_num[1], sum]
+  end
+  two_num.last
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,8 +22,14 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 55"
   puts "=>", fibonacci(10)
 
+  puts
+
+  puts "Expecting: 3"
+  puts "=>", fibonacci(4)
+
   # Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
+# array with two values then iterate through the sums
 # And a written explanation of your solution
