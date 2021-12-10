cb3107f7595e0d9d01e5e378cfea5c7dd557f9d5
+ <+>
2021-12-10 16:06:38 -0500

    week 1 day 4 js

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,5 +1,12 @@
 function selectionSort(arr) {
-  // type your code here
+  const sortArray = []
+  while (arr.length > 0){
+    let minimun = Math.min(...arr)
+    let index = arr.indexOf(minimun)
+    sortArray.push(minimun)
+    arr.splice(index, 1)
+  }
+  return sortArray
 }
 
 if (require.main === module) {
@@ -9,15 +16,27 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: [-7, 7, 77, 777]");
+  console.log("=>", selectionSort([77, 777, 7, -7]));
+
   // BENCHMARK HERE, and print the average runtime
   const longInput = [];
 
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+  
+  const startTime = Date.now()
+  for (let i = 0; i < 1000; ++i){
+    selectionSort([2, 1])
+    selectionSort(longInput)
+  }
+  const avgTime = (Date.now() - startTime) / 2000
+  console.log(avgTime)
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+// make empty array for sorted result, get the minimums out of input array in to sorted array in order
 // And a written explanation of your solution
