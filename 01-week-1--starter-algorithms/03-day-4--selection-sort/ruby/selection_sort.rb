60ef1b0830da4ffbd74180cfa98499835ac0227e
+ <+>
2021-12-10 16:17:16 -0500

    week 1 day 4 rb

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
@@ -1,5 +1,12 @@
 def selection_sort(arr)
-  # type your code in here
+  sortArray = []
+  until arr.length == 0
+    minimum = arr.min
+    index = arr.index(minimum)
+    sortArray << minimum
+    arr.delete_at(index)
+  end
+  sortArray
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -9,12 +16,24 @@ if __FILE__ == $PROGRAM_NAME
 
   puts
 
+  puts "Expecting: [-7, 7, 77, 777]"
+  print "=> "
+  print selection_sort([77, 777, 7, -7])
+
   # Don't forget to add your own!
 
   # BENCHMARK HERE, you can print the average runtime
   long_input = []
 
   100.times { long_input << rand }
+
+  start_time = Time.now
+  1000.times do
+    selection_sort([2, 1])
+    selection_sort(long_input)
+  end
+  avg_time = (Time.now - start_time) / 2000
+  puts avg_time
 end
 
 # Please add your pseudocode to this file
