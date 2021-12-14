2651be068d8b383dd416403065bc634366f60a0d
+ <+>
2021-12-14 16:40:58 -0500

    week 2 day 2 rb

--- 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
+++ 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
@@ -1,5 +1,7 @@
 def recursive_search(arr, target)
-  # type your code in here
+  return false if arr.empty?
+  return true if arr.first == target
+  recursive_search(arr[1..-1], target)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -10,6 +12,16 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: false"
   puts "=>", recursive_search([3, 2, 1], 4)
+
+  puts
+
+  puts "Expecting: true"
+  puts "=>", recursive_search([7, 77, 777], 77)
+
+  puts
+
+  puts "Expecting: false"
+  puts "=>", recursive_search([3, 21, 77], 7)
 
   # Don't forget to add your own!
 end
