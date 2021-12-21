1d736a6a4f2a13a1616632bd3b118459f45c41ce
+ <+>
2021-12-21 16:33:09 -0500

    week 3 day 2 js

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,32 @@
 function romanNumeral(string) {
-  // type your code here
+  const baseRomans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  }
+  let total = 0
+  let index = 0
+  while (index < string.length){
+    const twoString = string[index] + (string[index + 1] || '')
+    if (baseRomans[twoString] !== undefined){
+      total += baseRomans[twoString]
+      index += 2
+    } else {
+      total += baseRomans[string[index]]
+      ++index  
+    }
+  }
+  return total
 }
 
 if (require.main === module) {
