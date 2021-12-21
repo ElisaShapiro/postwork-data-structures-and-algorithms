5977db1f23fb376d310d4eb7f34514b20230e3d6
+ <+>
2021-12-21 16:45:18 -0500

    week 3 day 2 rb

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/ruby/roman_numeral.rb
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/ruby/roman_numeral.rb
@@ -1,5 +1,32 @@
 def roman_numeral(string)
-  # type your code in here
+  baseRomans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  }
+  total = 0
+  index = 0
+  while index < string.length
+    twoString = (string[index] + (string[index + 1] || '')).to_sym
+    if !baseRomans[twoString].nil?
+      total += baseRomans[twoString]
+      index += 2
+    else
+      total += baseRomans[string[index].to_sym]
+      index +=1
+    end
+  end
+  total
 end
 
 if __FILE__ == $PROGRAM_NAME
